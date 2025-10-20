ea28f6a4d8e0ee9f76c645b095c7fc0eb3b31981
David Ngugi <ndavidngugi@gmail.com>
2025-10-20 13:18:17 +0300

    Bump version to v1.0.4

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-VERSION="1.0.3"
+VERSION="1.0.4"
 REPO_URL="https://raw.githubusercontent.com/DavidNgugi/speed-cli/main"
 INSTALL_DIR="$HOME/scripts"
 LOG_DIR="$HOME/internet_logs"
